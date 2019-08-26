421539b6e4f7c169ae8c7d11afab22c59d5b8a00
xuemingqin <1208280008@qq.com>
2019-08-26 21:46:16 +0900

    delete file

--- projects/Interphone/DianpingSam/app/src/main/java/com/example/dianpingsam/activity/StartActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.dianpingsam.activity;
-
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
-
-public class StartActivity extends AppCompatActivity{
-
-}
